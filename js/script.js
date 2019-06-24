894d8f56e8d5b12d65f9052004f9b2092247bcc5
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-06-24 13:04:55 +0200

    Fix bugs with navbar, section premium and backgrounds

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
-var mobileButton = document.querySelector('.mobile');
 var navigation = document.querySelector('.nav');
-var overlay = document.querySelector('.overlay');
 var burgerMenu = document.querySelector('.mobile');
 
 
 function showMenu() {
-	if(navigation.classList.contains('visible')) {
-		navigation.classList.remove('visible');
-		overlay.classList.remove('dark');
-		burgerMenu.classList.remove('active');
-	} else {
-		navigation.classList.add('visible');
-		overlay.classList.add('dark');
-		burgerMenu.classList.add('active');
-	}
+	navigation.classList.toggle('visible');
+	burgerMenu.classList.toggle('active');
 }
 
-mobileButton.addEventListener('click', showMenu);
+burgerMenu.addEventListener('click', showMenu);
