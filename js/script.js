0ecd5677d2756180c04f8b2894de713791194a9c
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-06-24 22:11:52 +0200

    Create scroll to top button

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ function showMenu() {
 }
 
 burgerMenu.addEventListener('click', showMenu);
+
+var scrollBtn = document.querySelector('.scroll-btn');
+
+function animateScroll() {
+	if(window.scrollY > 0) {
+		window.scrollBy(0, -20);
+		setTimeout(animateScroll, 1);
+	} 
+}
+
+scrollBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	animateScroll();
+
+});
